192d8b57de338e24965a6a43d12e10c2192760a8
Alexander Åhlander <ahlander.alexander@gmail.com>
2015-04-09 12:29:59 +0200

    added new mainfile for discdatabase

---
+++ servermaindisc.cc
@@ -0,0 +1,20 @@
+#include "dbinterface.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc,char *argv[]){
+	int port = 4242;
+	if(argc > 2){
+		cerr<<"Too many arguments. Only enter port";
+		return 1;
+	}
+	if(argc == 2){
+		port = static_cast<int>(*argv[1]);
+	}
+	DBInterface db(port, true);
+	db.startServer();
+
+	return 0;
+}
